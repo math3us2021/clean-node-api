a28100a82be60dfd09509c1fca2380106fff26d7
matheus <matheusbf.mf@gmail.com>
2024-06-30 21:48:21 -0300

    feat: ensure SingUpController return 400 if an invalid email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,14 +2,26 @@ import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { MissingParamsError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helpers'
 import { type Controller } from '../protocols/controller'
+import { type EmailValidator } from '../protocols/email-validator'
+import {InvalidParamsError} from "../errors/invalid-params-error";
 
 export class SingUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredField) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamsError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email as string)
+    if (!isValid) {
+      return badRequest(new InvalidParamsError('email'))
     }
     return {
       statusCode: 200,
